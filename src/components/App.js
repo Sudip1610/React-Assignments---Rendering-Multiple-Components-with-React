1931e1e26d3226d00ac4dd789fa6a0f3fb773250
Sudip Ghosh <ghoshsudip084@gmail.com>
2020-11-05 20:38:34 +0530

    Change 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <></>
-  )
-}
-
+    <>
+      <h3 data-ns-test="project-name">React app</h3>
+      <h4 data-ns-test="project-description">Lots of Reactive apps</h4>
+      <h3 data-ns-test="project-name">Food app</h3>
+      <h4 data-ns-test="project-description">Realtime food delivery app</h4>
+    </>
+  );
+};
 
 export default App;
